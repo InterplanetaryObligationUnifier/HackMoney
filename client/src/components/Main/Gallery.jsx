d6c51ac9b6fefafe2df36bccff5a2080ea863b6c
danielforkner <daniel.forkner@gmail.com>
2022-05-19 19:30:57 -0400

    can mint a custom NFT

--- client/src/components/Main/Gallery.jsx
+++ client/src/components/Main/Gallery.jsx
@@ -1,85 +1,136 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useWeb3 from '../hooks/useWeb3';
 import Nft_init from '../../contracts/Nft_init.json';
-import { NFTStorage } from "nft.storage";
+import { NFTStorage } from 'nft.storage';
 
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Gallery = () => {
   const { web3, walletAddress, nfts, setNfts } = useWeb3();
-  const token = new web3.eth.Contract(
-    Nft_init.abi,
-    Nft_init.networks[5777].address
-  );
-  const getExampleImage = async () => {
-    const imageOriginUrl = "https://user-images.githubusercontent.com/87873179/144324736-3f09a98e-f5aa-4199-a874-13583bf31951.jpg"
-    try {
-      const response = await fetch(imageOriginUrl, { mode: 'no-cors' })
-      console.log("Here is the returned blob", response.blob)
-      return "TestImage.png"
-    } catch (error) {
-      console.error(error)
-    }
+  const [mintForm, setMintForm] = useState(false);
+  const [nftName, setNftName] = useState('');
+  const [nftDescription, setNftDescription] = useState('');
+  const [nftImg, setNftImg] = useState('');
+
+  let token;
+  if (web3) {
+    token = new web3.eth.Contract(
+      Nft_init.abi,
+      Nft_init.networks[5777].address
+    );
   }
-  const handleClick = async () => {
-    const image = await getExampleImage()
-    console.log("Your image is this blob", image)
+
+  const getImage = async (img) => {
+    const imageOriginUrl = img;
+    try {
+      const response = await fetch(imageOriginUrl);
+      console.log('response', response);
+      const image = await response.blob();
+      return image;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleClick = () => setMintForm(!mintForm);
+
+  const mint = async () => {
+    const image = await getImage(nftImg);
+    console.log('Your image is this blob', image);
     try {
       const nftToStore = {
-        image, 
-        name: "Hard coded",
-        description: "Hard coded",
+        image,
+        name: nftName,
+        description: nftDescription,
         properties: {},
-      }
-      console.log(API_KEY, "this is your API key")
-      const client = new NFTStorage({ token: API_KEY })
-      const metadata = await client.store(nftToStore)
-      console.log('Metadata URI', metadata.url)
+      };
+      console.log(API_KEY, 'this is your API key');
+      const client = new NFTStorage({ token: API_KEY });
+      const metadata = await client.store(nftToStore);
+      console.log('Metadata URI', metadata.url);
       const event = await token.methods
-        .awardItem(
-          walletAddress,
-          metadata.url
-        )
+        .awardItem(walletAddress, metadata.url)
         .send({
           from: walletAddress,
         });
       console.log('event!: ', event);
       const tokenId = event.events.Transfer.returnValues.tokenId;
       const uri = await token.methods.tokenURI(tokenId).call();
-      setNfts([...nfts, uri])
+      setNfts([...nfts, uri]);
     } catch (error) {
       console.error(error);
     }
   };
+
   useEffect(() => {
-    const getNfts = async() => {
-      const owned = await token.methods.balanceOf(walletAddress).call()
+    if (!web3) return;
+    const getNfts = async () => {
+      const owned = await token.methods.balanceOf(walletAddress).call();
       let tokenIds = [];
-      for (let i = 0; i < owned ; i++){
-        tokenIds.push(await token.methods.tokenOfOwnerByIndex(walletAddress, i).call())
+      for (let i = 0; i < owned; i++) {
+        tokenIds.push(
+          await token.methods.tokenOfOwnerByIndex(walletAddress, i).call()
+        );
       }
       let ownerURIs = [];
       for (let tokenId of tokenIds) {
-        console.log(tokenId)
-        ownerURIs.push(await token.methods.tokenURI(tokenId).call())
+        console.log(tokenId);
+        ownerURIs.push(await token.methods.tokenURI(tokenId).call());
       }
-      let jasons = []
+      let jasons = [];
       for (let uri of ownerURIs) {
-        const parsed = uri.slice(7)
-        const fetched = await fetch(`https://ipfs.io/ipfs/${parsed}`)
-        const data = await fetched.json()
-        data.image = `https://ipfs.io/ipfs/${data.image.split('/')[2]}/blob.png`
-        jasons.push(data)
+        const parsed = uri.slice(7);
+        const fetched = await fetch(`https://ipfs.io/ipfs/${parsed}`);
+        const data = await fetched.json();
+        data.image = `https://ipfs.io/ipfs/${data.image.split('/')[2]}/blob`;
+        jasons.push(data);
       }
-      console.log("Here are the jasons", jasons)
-      setNfts(jasons)
-    }
+      console.log('Here are the jasons', jasons);
+      setNfts(jasons);
+    };
 
-      getNfts()
-  },[])
+    getNfts();
+  }, [web3]);
+
   return (
     <div>
       <button onClick={handleClick}>MINT THE NFTS</button>
+      {mintForm && (
+        <div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              mint();
+            }}
+          >
+            <label htmlFor="nftName">Nft Name</label>
+            <input
+              name="nftName"
+              type="text"
+              required={true}
+              value={nftName}
+              onChange={(e) => setNftName(e.target.value)}
+            />
+            <label htmlFor="nftDescription">Nft Description</label>
+            <input
+              name="nftDescription"
+              type="text"
+              required={true}
+              value={nftDescription}
+              onChange={(e) => setNftDescription(e.target.value)}
+            />
+            <label htmlFor="nftImg">Nft ImgURL</label>
+            <input
+              name="nftImg"
+              type="text"
+              required={true}
+              value={nftImg}
+              onChange={(e) => setNftImg(e.target.value)}
+            />
+            <button type="submit">MINT!</button>
+          </form>
+        </div>
+      )}
+
       <div className="glarrery">
         {nfts.map((nft, i) => {
           return (
